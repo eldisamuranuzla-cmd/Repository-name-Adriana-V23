99fdb758b41010427b6e34495f389d58e8cbc9c6
eldisamuranuzla-cmd <eldisamuranuzla@gmail.com>
2026-09-11 00:03:56 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='adriana-v24.1';
-const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./astra-agro-logo.png','./astra-agro-logo-compact.png','./adriana-logo-main.png','./kpi-tbs.png','./kpi-worker.png','./kpi-truck.png','./kpi-bjr.png'];
+const CACHE='adriana-v22.5-migration-migration';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./astra-agro-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
 self.addEventListener('fetch',e=>e.respondWith(fetch(e.request).then(r=>{const copy=r.clone();if(e.request.method==='GET')caches.open(CACHE).then(c=>c.put(e.request,copy));return r}).catch(()=>caches.match(e.request))));
