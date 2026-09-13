5e7e0f682d6e26c2daf96fff81bfaecd01ceba6a
eldisamuranuzla-cmd <eldisamuranuzla@gmail.com>
2026-09-13 13:59:12 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='adriana-v22.5-migration-migration';
+const CACHE='adriana-v24-2-adaptive-h1';
 const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png','./astra-agro-logo.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
